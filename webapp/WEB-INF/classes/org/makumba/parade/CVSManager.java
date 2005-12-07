36fa4be8c7a0e954af595d7bca9c5e00280ef7b4
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-12-07 17:40:41 +0000

    Basic manager data generation

--- webapp/WEB-INF/classes/org/makumba/parade/CVSManager.java
+++ webapp/WEB-INF/classes/org/makumba/parade/CVSManager.java
@@ -1,12 +1,68 @@
 package org.makumba.parade;
 
-import org.makumba.parade.ifc.DirectoryRefresher;
-import org.makumba.parade.model.Row;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
-public class CVSManager implements DirectoryRefresher {
+import org.makumba.parade.ifc.DirectoryRefresher;
+import org.makumba.parade.ifc.RowRefresher;
+import org.makumba.parade.model.DirCVS;
+import org.makumba.parade.model.File;
+import org.makumba.parade.model.FileCVS;
+import org.makumba.parade.model.Row;
+import org.makumba.parade.model.RowCVS;
+
+public class CVSManager implements DirectoryRefresher, RowRefresher {
 
 	public void directoryRefresh(Row row, String path) {
-		// TODO Auto-generated method stub
+		
+		Iterator i = row.getFiles().keySet().iterator();
+		while(i.hasNext()) {
+			File file = (File) row.getFiles().get(i.next());
+			Map filedata = file.getFiledata();
+			if(filedata == null) {
+				filedata = new HashMap();
+			}
+			
+			if(file.getIsDir()) {
+				DirCVS dircvsdata = new DirCVS();
+				dircvsdata.setManagername("dircvs");
+				dircvsdata.setFile(file);
+				
+//				do something useful
+				
+				filedata.put("dircvs",dircvsdata);
+				file.setFiledata(filedata);
+				
+			} else {
+				FileCVS filecvsdata = new FileCVS();
+				filecvsdata.setManagername("filecvs");
+				filecvsdata.setFile(file);
+				
+//				do something useful
+				
+				filedata.put("filecvs",filecvsdata);
+				file.setFiledata(filedata);
+			}
+		}
+		
+	}
+
+	public void rowRefresh(Row row) {
+		
+		/* Some example data */
+		RowCVS cvsdata = new RowCVS();
+		cvsdata.setDataType("cvs");
+		cvsdata.setRow(row);
+		
+		Map rowdata = row.getRowdata();
+		if(rowdata == null) {
+			rowdata = new HashMap();
+		}
+		
+		rowdata.put("cvs",cvsdata);
+		row.setRowdata(rowdata);
+		
 		
 	}
 
--- webapp/WEB-INF/classes/org/makumba/parade/TrackerManager.java
+++ webapp/WEB-INF/classes/org/makumba/parade/TrackerManager.java
@@ -1,14 +1,36 @@
 package org.makumba.parade;
 
 
-import org.makumba.parade.ifc.DirectoryRefresher;
-import org.makumba.parade.model.File;
-import org.makumba.parade.model.Row;
+import java.util.HashMap;
+import java.util.Map;
 
-public class TrackerManager implements DirectoryRefresher {
+import org.makumba.parade.ifc.DirectoryRefresher;
+import org.makumba.parade.ifc.RowRefresher;
+import org.makumba.parade.model.Row;
+import org.makumba.parade.model.RowTracker;
+
+public class TrackerManager implements DirectoryRefresher, RowRefresher {
 
 	public void directoryRefresh(Row row, String path) {
 		// TODO Auto-generated method stub
+		
+	}
+
+	public void rowRefresh(Row row) {
+		// TODO Auto-generated method stub
+		
+		/* Some example data */
+		RowTracker trackerdata = new RowTracker();
+		trackerdata.setDataType("tracker");
+		trackerdata.setRow(row);
+		
+		Map rowdata = row.getRowdata();
+		if(rowdata == null) {
+			rowdata = new HashMap();
+		}
+				
+		rowdata.put("tracker",trackerdata);
+		row.setRowdata(rowdata);
 		
 	}
 	
