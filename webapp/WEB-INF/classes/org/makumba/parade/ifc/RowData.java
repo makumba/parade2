3074670bfb2eed62a3b7411ed9ef98e608330fd6
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-12-08 16:33:38 +0000

    Common interface for all the Row data objects

---
+++ webapp/WEB-INF/classes/org/makumba/parade/ifc/RowData.java
@@ -0,0 +1,12 @@
+package org.makumba.parade.ifc;
+
+import org.makumba.parade.model.Row;
+
+public interface RowData {
+
+	public String getDataType();
+	
+	public Row getRow();
+	
+	public void setRow(Row row);
+}
