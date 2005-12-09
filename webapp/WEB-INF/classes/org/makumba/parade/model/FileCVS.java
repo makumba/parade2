cd4a21fabe04ee08b988e3977dbecb26c79fe199
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-12-09 22:24:24 +0000

    Added CVS fields

--- webapp/WEB-INF/classes/org/makumba/parade/model/FileCVS.java
+++ webapp/WEB-INF/classes/org/makumba/parade/model/FileCVS.java
@@ -1,9 +1,53 @@
 package org.makumba.parade.model;
 
+import java.util.Date;
+
 public class FileCVS extends AbstractFileData {
 	
 	private Long id;
 	
+	private Integer status;
+	
+	private String revision;
+	
+	private Date date;
+	
+	private boolean isCVSDir;
+	
+	
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public boolean getIsCVSDir() {
+		return isCVSDir;
+	}
+
+	public void setIsCVSDir(boolean isCVSDir) {
+		this.isCVSDir = isCVSDir;
+	}
+
+	public String getRevision() {
+		return revision;
+	}
+
+	public void setRevision(String revision) {
+		this.revision = revision;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
 	public Long getId() {
 		return id;
 	}
--- webapp/WEB-INF/classes/org/makumba/parade/model/RowCVS.java
+++ webapp/WEB-INF/classes/org/makumba/parade/model/RowCVS.java
@@ -5,6 +5,36 @@ import org.makumba.parade.ifc.RowData;
 public class RowCVS extends AbstractRowData implements RowData {
 	
 	private Long id;
+	
+	private String user;
+	
+	private String module;
+	
+	private String branch;
+
+	public String getBranch() {
+		return branch;
+	}
+
+	public void setBranch(String branch) {
+		this.branch = branch;
+	}
+
+	public String getModule() {
+		return module;
+	}
+
+	public void setModule(String module) {
+		this.module = module;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
 
 	public Long getId() {
 		return id;
