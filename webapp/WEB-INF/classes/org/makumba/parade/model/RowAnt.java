ebf5bbeea83af16d86ecd1d703d6864f805f18d7
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-12-12 09:06:34 +0000

    Updated fields

--- webapp/WEB-INF/classes/org/makumba/parade/model/RowAnt.java
+++ webapp/WEB-INF/classes/org/makumba/parade/model/RowAnt.java
@@ -1,10 +1,35 @@
 package org.makumba.parade.model;
 
+import java.util.ArrayList;
+
+
 import org.makumba.parade.ifc.RowData;
 
 public class RowAnt extends AbstractRowData implements RowData {
 
 	private Long id;
+	
+	private Long lastmodified;
+	
+	private ArrayList topTargets = new ArrayList();
+	
+	private ArrayList subTargets = new ArrayList();
+	
+	public ArrayList getSubTargets() {
+		return subTargets;
+	}
+
+	public void setSubTargets(ArrayList subTargets) {
+		this.subTargets = subTargets;
+	}
+
+	public ArrayList getTopTargets() {
+		return topTargets;
+	}
+
+	public void setTopTargets(ArrayList topTargets) {
+		this.topTargets = topTargets;
+	}
 
 	public Long getId() {
 		return id;
@@ -12,6 +37,14 @@ public class RowAnt extends AbstractRowData implements RowData {
 
 	public void setId(Long id) {
 		this.id = id;
+	}
+
+	public Long getLastmodified() {
+		return lastmodified;
+	}
+
+	public void setLastmodified(Long lastmodified) {
+		this.lastmodified = lastmodified;
 	}
 	
 	
