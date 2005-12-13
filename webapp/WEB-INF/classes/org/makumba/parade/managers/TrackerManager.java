ff9a3c740261189d2dced7ce0bb103c89ab1f446
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-12-13 18:25:55 +0000

    Added status

--- webapp/WEB-INF/classes/org/makumba/parade/managers/TrackerManager.java
+++ webapp/WEB-INF/classes/org/makumba/parade/managers/TrackerManager.java
@@ -11,6 +11,22 @@ import org.makumba.parade.model.Row;
 
 public class TrackerManager implements DirectoryRefresher {
 
+	//"Specification"=0,"DesignApproved"=1,"Started"=2,"Developing"=3,"Stable"=4,"Frozen"=5,"Dropped"=6
+	
+	static Integer SPECIFICATION = new Integer(0);
+
+    static Integer DESIGNAPPROVED = new Integer(1);
+
+    static Integer STARTED = new Integer(2);
+
+    static Integer DEVELOPING = new Integer(3);
+
+    static Integer STABLE = new Integer(4);
+
+    static Integer FROZEN = new Integer(5);
+
+    static Integer DROPPED = new Integer(6);
+	
 	public void directoryRefresh(Row row, String path) {
 		
 		File currFile = (File) row.getFiles().get(path);
