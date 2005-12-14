177c5a8225d3ce0ad315b5d4f780256c2be10a5e
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-12-14 18:09:37 +0000

    Added WebappManager fields

--- webapp/WEB-INF/classes/org/makumba/parade/model/RowWebapp.java
+++ webapp/WEB-INF/classes/org/makumba/parade/model/RowWebapp.java
@@ -5,6 +5,36 @@ import org.makumba.parade.ifc.RowData;
 public class RowWebapp extends AbstractRowData implements RowData {
 	
 	private Long id;
+	
+	private String path;
+	
+	private String name;
+	
+	private Integer status;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
 
 	public Long getId() {
 		return id;
