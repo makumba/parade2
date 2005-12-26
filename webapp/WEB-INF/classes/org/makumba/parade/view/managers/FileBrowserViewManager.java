3e594ed93563502689e8e9772e7392caf659f5fb
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-12-26 20:49:50 +0000

    Improved listing of files and dirs

--- webapp/WEB-INF/classes/org/makumba/parade/view/managers/FileBrowserViewManager.java
+++ webapp/WEB-INF/classes/org/makumba/parade/view/managers/FileBrowserViewManager.java
@@ -2,9 +2,16 @@ package org.makumba.parade.view.managers;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.StringTokenizer;
 
+import org.makumba.parade.model.File;
 import org.makumba.parade.model.Parade;
 import org.makumba.parade.model.Row;
+import org.makumba.parade.model.managers.FileManager;
+import org.makumba.parade.tools.FileComparator;
 import org.makumba.parade.view.interfaces.FileBrowserView;
 
 public class FileBrowserViewManager implements FileBrowserView {
@@ -13,40 +20,70 @@ public class FileBrowserViewManager implements FileBrowserView {
 		StringWriter result = new StringWriter();
 		PrintWriter out = new PrintWriter(result);
 		
-		if(path==null) path="/";
-		path=path.replace(java.io.File.separatorChar, '/');
-		if(path.startsWith("/")) path=path.substring(1);
-		if(path.length()>1&& !path.endsWith("/")) path=path+"/";
+		//if this is the root of the row
+		if(path==null) path=new java.io.File(r.getRowpath()).getAbsolutePath();
 		
 		FileViewManager fileV = new FileViewManager();
 		CVSViewManager cvsV = new CVSViewManager();
-		
 		
 		out.println("<HTML><HEAD><TITLE>"+r.getRowname()+" files</TITLE>"+
 				"</HEAD><BODY>");
 		
 		out.println("<p align='left'" +
-					"<font size=+1>[<a href='/servlet/file?context="+r.getRowname()+
-					"&path="+path+"'>"+r.getRowname()+"</a>]"+path+
+					"<font size=+1>[<a href='/servlet/file?context="+r.getRowname()+"'>"+
+					r.getRowname()+"</a>]"+getParentDir(r,path)+
 					"</font>");
 		out.println("<img src='/images/folder-open.gif'>"+
 					"<br><font size=-2>"+r.getRowpath()+"</font>");
 		out.println("<table border='0' width='100%' cellspacing='0' cellpadding='2'>");
 		
+		out.println("</p>");
+		
 		// headers
 		out.println("<tr bgcolor=#ddddff>" +
-					"<td align='center'>"+fileV.getFileViewHeader()+"</td>" +
-					"<td align='center'>"+cvsV.getFileViewHeader()+"</td>" +
+					fileV.getFileViewHeader()+
+					cvsV.getFileViewHeader()+
 					"</tr>");
 		
 		//files
+		List files = FileManager.getChildren(r,path);
+		FileComparator fc = new FileComparator();
+		
+		Collections.sort(files, fc);
+		
+		int counter = 0;
+		for(Iterator j = files.iterator(); j.hasNext();) {
+			File currentFile = (File) j.next();
+			out.println("<tr bgcolor="+(((counter % 2) == 0) ? "#ffffff" : "#f5f5ff")+" valign=top>" +
+						fileV.getFileView(r,currentFile)+
+						cvsV.getFileView(r,currentFile)+
+						"</td></tr>");
+			
+			counter++;
+		}
 		
 		
 		
-		out.println("</p>");
+		
 		out.println("</TABLE></BODY></HTML>");
 		
 		return result.toString();
+	}
+
+	private String getParentDir(Row r,String path) {
+		String absoluteRowPath = new java.io.File(r.getRowpath()).getAbsolutePath();
+		String relativePath = path.substring(absoluteRowPath.length(), path.length());
+		String parentDir = "";
+		String currentPath = path.substring(0,absoluteRowPath.length());
+		
+		StringTokenizer st = new StringTokenizer(relativePath,java.io.File.separator);
+		while(st.hasMoreTokens()) {
+			String thisToken = st.nextToken();
+			currentPath+=java.io.File.separator + thisToken; 
+			parentDir+="<a href='/servlet/file?context="+r.getRowname()+
+						"&path="+currentPath+"'>"+thisToken+"</a>"+"/";
+		}
+		return parentDir;
 	}
 
 	
