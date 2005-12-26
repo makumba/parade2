65eb8a8a0564f6cc8fd0c32009f2263d12853b52
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2005-12-26 20:47:30 +0000

    Comparator to compare file File-s and dir File-s

---
+++ webapp/WEB-INF/classes/org/makumba/parade/tools/FileComparator.java
@@ -0,0 +1,31 @@
+package org.makumba.parade.tools;
+
+import java.util.Comparator;
+
+import org.makumba.parade.model.File;
+
+public class FileComparator implements Comparator{
+	
+
+    public FileComparator() {}
+
+    public int compare(Object d1, Object d2) {
+    	
+    	File f1 = (File) d1;
+    	File f2 = (File) d2;
+
+    	if(f1.getIsDir() && !f2.getIsDir()) return -1;
+    	if(!f1.getIsDir() && f2.getIsDir()) return 1;
+    	
+    	if(f1.getIsDir() && f2.getIsDir()) {
+    		String s1 = f1.getName();
+    		String s2 = f2.getName();
+    		return s1.compareTo(s2);
+    	}
+    	
+    	return 0;
+       
+        
+    }
+} 
+
