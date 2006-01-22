9ded004627f830a88f7e748f122071660175ebc7
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2006-01-22 18:20:53 +0000

    Formatting source & refactoring imports

--- webapp/WEB-INF/classes/org/makumba/parade/init/InitServlet.java
+++ webapp/WEB-INF/classes/org/makumba/parade/init/InitServlet.java
@@ -15,98 +15,92 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.tool.hbm2ddl.SchemaUpdate;
 import org.makumba.parade.model.Parade;
-import org.makumba.parade.view.managers.ViewManager;
+import org.makumba.parade.view.managers.RowDisplay;
 
+public class InitServlet extends HttpServlet implements Runnable {
 
-public class InitServlet extends HttpServlet  implements Runnable {
-	
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	static Logger logger = Logger.getLogger(InitServlet.class.getName());
-	
+    static Logger logger = Logger.getLogger(InitServlet.class.getName());
+
     private static Configuration cfg;
-    
+
     private static SessionFactory sessionFactory = null;
-    
+
     private static Long one = new Long(1);
-    
+
     private Session session = null;
-    
+
     private Parade p = null;
-    
+
     {
-    	/* Initializing Hibernate */
-    	try {
-        	
+        /* Initializing Hibernate */
+        try {
+
             cfg = new Configuration().configure();
             cfg.addResource("org/makumba/parade/model/AbstractFileData.hbm.xml");
             cfg.addResource("org/makumba/parade/model/Parade.hbm.xml");
             cfg.addResource("org/makumba/parade/model/Row.hbm.xml");
             cfg.addResource("org/makumba/parade/model/AbstractRowData.hbm.xml");
             cfg.addResource("org/makumba/parade/model/File.hbm.xml");
-            
-        	sessionFactory = cfg.buildSessionFactory();
-            
+
+            sessionFactory = cfg.buildSessionFactory();
+
             SchemaUpdate schemaUpdate = new SchemaUpdate(cfg);
             schemaUpdate.execute(true, true);
-            
+
         } catch (Throwable t) {
-            logger.error(t); t.printStackTrace();
+            logger.error(t);
+            t.printStackTrace();
         }
-    	
+
     }
-        
-        public void init(ServletConfig conf) throws ServletException
-        {
-             new Thread(this).start();
+
+    public void init(ServletConfig conf) throws ServletException {
+        new Thread(this).start();
+    }
+
+    public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println("init: Starting Parade initialization at " + new java.util.Date());
+        long start = System.currentTimeMillis();
+        System.out.flush();
+
+        /* Getting Parade, creating one if none found */
+
+        session = sessionFactory.openSession();
+
+        Transaction tx = session.beginTransaction();
+
+        p = (Parade) session.get(Parade.class, new Long(1));
+        if (p == null) {
+            p = new Parade();
+            p.setId(one);
+
+            session.save(p);
         }
 
-        public void run()
-        {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            System.out.println("init: Starting Parade initialization at " + new java.util.Date());
-        	long start = System.currentTimeMillis();
-            System.out.flush();
-        	        	    
-            /* Getting Parade, creating one if none found */
-            
-    		session = sessionFactory.openSession();
-    		
-    		Transaction tx = session.beginTransaction();
-    		        		
-            p = (Parade) session.get(Parade.class, new Long(1));
-            if (p == null) {
-            	p = new Parade();
-            	p.setId(one);
-            	
-            	session.save(p);
-            }
-            
-            p.refresh();
-            
-            tx.commit();
-            
-            
-            
-            session.close();
-            
-            
-            System.out.println("INFO: Launching ParaDe finished at "+new java.util.Date());
-            long end = System.currentTimeMillis();
-            
-            long refresh = end-start;
-            System.out.println("INFO: Initialisation took "+refresh+" ms");
-            
-        }
-        
-        public static SessionFactory getSessionFactory() {
-        	return sessionFactory;
-        }
+        p.refresh();
 
+        tx.commit();
+
+        session.close();
+
+        System.out.println("INFO: Launching ParaDe finished at " + new java.util.Date());
+        long end = System.currentTimeMillis();
+
+        long refresh = end - start;
+        System.out.println("INFO: Initialisation took " + refresh + " ms");
+
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
 
 }
--- webapp/WEB-INF/classes/org/makumba/parade/init/ParadeProperties.java
+++ webapp/WEB-INF/classes/org/makumba/parade/init/ParadeProperties.java
@@ -10,44 +10,42 @@ import java.util.StringTokenizer;
 import org.apache.log4j.Logger;
 
 public class ParadeProperties {
-	
-	static public String paradeBase = "." + java.io.File.separator;
-	static String fileName = paradeBase + "parade.properties";
-	
-	private static Properties config;
-	
-	static public String paradeBaseRelativeToTomcatWebapps = ".."
-        + File.separator + "..";
-	
-	static Logger logger = Logger.getLogger(ParadeProperties.class.getName());
-	
-	static {
-		
+
+    static public String paradeBase = "." + java.io.File.separator;
+
+    static String fileName = paradeBase + "parade.properties";
+
+    private static Properties config;
+
+    static public String paradeBaseRelativeToTomcatWebapps = ".." + File.separator + "..";
+
+    static Logger logger = Logger.getLogger(ParadeProperties.class.getName());
+
+    static {
+
         try {
             config = new Properties();
             config.load(new FileInputStream(fileName));
         } catch (Throwable t) {
-            logger.error("Error while loading parade.properties",t);
+            logger.error("Error while loading parade.properties", t);
         }
     }
-	
-	public static String getProperty(String configProperty) {
+
+    public static String getProperty(String configProperty) {
         return config.getProperty(configProperty);
     }
-	
-	public static List getElements(String configProperty) {
-		List l = new LinkedList();
-		
-		String s = getProperty(configProperty);
-		if(s == null)
-			return null;
-		StringTokenizer st = new StringTokenizer(s,",");
-		while(st.hasMoreElements()) {
-			l.add(((String)st.nextToken()).trim());
-		}
-		return l;
-	}
-	
-	
-	
+
+    public static List getElements(String configProperty) {
+        List l = new LinkedList();
+
+        String s = getProperty(configProperty);
+        if (s == null)
+            return null;
+        StringTokenizer st = new StringTokenizer(s, ",");
+        while (st.hasMoreElements()) {
+            l.add(((String) st.nextToken()).trim());
+        }
+        return l;
+    }
+
 }
