80c21d74cfb737b779ea6fbefccd4817d2ac0a5b
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2006-05-30 21:02:57 +0000

    Fixing wrong copying of files

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-cp -r tomcat/*.* tomcat-fresh/
+cp -r tomcat/* tomcat-fresh
 mkdir -p tomcat-fresh/common/classes/org/makumba/parade/tools
 ant compile
 cp webapp/WEB-INF/classes/org/makumba/parade/tools/TriggerFilter*.class tomcat-fresh/common/classes/org/makumba/parade/tools/
 cp webapp/WEB-INF/classes/org/makumba/parade/tools/HttpServletRequestDummy*.class tomcat-fresh/common/classes/org/makumba/parade/tools/
-mkdir -p tomcat-fresh/webapps/parade
-mkdir -p tomcat-fresh/webapps_dummy
+mkdir  tomcat-fresh/webapps/parade
+mkdir  tomcat-fresh/webapps_dummy
 cp -r webapp/* tomcat-fresh/webapps/parade
 cp *.properties tomcat-fresh/
 cp lib/mysql.jar lib/ant-1.6.5.jar lib/ant-launcher.jar tomcat-fresh/webapps/parade/WEB-INF/lib/
-
-mv tomcat-fresh tomcat-parade
+mv tomcat-fresh/ tomcat-parade/
