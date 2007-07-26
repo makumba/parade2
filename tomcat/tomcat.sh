82220a33cd8acfc3c2a44a8eac3693f5420d5990
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2007-07-26 14:59:55 +0000

    Adding library path so that tomcat can find jnotify libs

--- tomcat/tomcat.sh
+++ tomcat/tomcat.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export JAVA_OPTS='-Xms70M -Xmx500M'
+export JAVA_OPTS='-Xms70M -Xmx500M -Djava.library.path=.'
 
 # if it's not already defined in your environment, specify the JAVA_HOME directory
 # export JAVA_HOME=''
