5af9401a05fc6f6a2b90526495a4e1a91787b713
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2007-10-09 13:24:37 +0000

    New statup scripts that should run parade2 "out of the box", only needing a tomcat 5.5 in ../tomcat

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+export CATALINA_HOME='tomcat'
+export CATALINA_BASE='../tomcat'
+export JAVA_OPTS='-Xms70M -Xmx500M -Dcatalina.base=tomcat-eclipse -Djava.io.tmpdir="${CATALINA_HOME}/temp" -Dcatalina.home="${CATALINA_HOME}" -Dfile.encoding=8859_1 -Djava.library.path=.'
+
+# if it's not already defined in your environment, specify the JAVA_HOME directory
+# export JAVA_HOME=''
+
+${CATALINA_BASE}/bin/startup.sh
