66ce313130b3658a79e79e40d57a11e803ad3894
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2007-10-10 13:18:51 +0000

    Removing useless scripts

--- tomcat/stop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# if it's not already defined in your environment, specify the JAVA_HOME directory
-# export JAVA_HOME=''
-
-bin/shutdown.sh
--- tomcat/tomcat.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export JAVA_OPTS='-Xms70M -Xmx500M -Djava.library.path=.'
-
-# if it's not already defined in your environment, specify the JAVA_HOME directory
-# export JAVA_HOME=''
-
-bin/startup.sh
-
-
