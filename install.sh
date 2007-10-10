a680939c35851db97c7033d192e10b5098fcbcd9
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2007-10-10 13:40:22 +0000

    Adapting script: it now sets up the tomcat environment

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 #!/bin/sh
 
-cp -r tomcat/* tomcat-fresh
-mkdir -p tomcat-fresh/common/classes/org/makumba/parade/tools
-ant compile
-cp webapp/WEB-INF/classes/org/makumba/parade/tools/TriggerFilter*.class tomcat-fresh/common/classes/org/makumba/parade/tools/
-cp webapp/WEB-INF/classes/org/makumba/parade/tools/HttpServletRequestDummy*.class tomcat-fresh/common/classes/org/makumba/parade/tools/
-mkdir  tomcat-fresh/webapps/parade
-mkdir  tomcat-fresh/webapps_dummy
-cp -r webapp/* tomcat-fresh/webapps/parade
-cp lib/mysql.jar lib/ant-1.6.5.jar lib/ant-launcher.jar tomcat-fresh/webapps/parade/WEB-INF/lib/
-cp build.* tomcat-fresh/
-cp jnotify.dll libjnotify.so tomcat-fresh/
-mv tomcat-fresh/ tomcat-parade
+export CATALINA_HOME=/home/projects/parade/util-java/apache-tomcat-5.5.20
+export CATALINA_BASE=.
+
+cp ${CATALINA_HOME}/server/lib/catalina-ant.jar ${ANT_HOME}/lib
+
+ant prepareTomcat
