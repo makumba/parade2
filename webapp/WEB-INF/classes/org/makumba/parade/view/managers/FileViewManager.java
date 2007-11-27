2a6836aa4adb3f96d5f56f7135661d9780cbae93
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2007-11-27 12:07:28 +0000

    Removing useless import

--- webapp/WEB-INF/classes/org/makumba/parade/view/managers/FileViewManager.java
+++ webapp/WEB-INF/classes/org/makumba/parade/view/managers/FileViewManager.java
@@ -5,7 +5,6 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
