a83d00080d028bcd9a6476c748e2657d2e2f1440
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2007-12-05 15:13:22 +0000

    Removing useless code

--- webapp/WEB-INF/classes/org/makumba/parade/tools/ParadeConsoleHandler.java
+++ webapp/WEB-INF/classes/org/makumba/parade/tools/ParadeConsoleHandler.java
@@ -8,13 +8,11 @@ public class ParadeConsoleHandler extends ConsoleHandler {
     
     @Override
     public void publish(LogRecord record) {
-        //TriggerFilter.redirectToServlet("perThreadEnabler", "perThreadEnabled", new Boolean(false));
         PerThreadPrintStream.setEnabled(false);
         Object[] params = {PerThreadPrintStream.get()};
         record.setParameters(params);
         super.publish(record);
         TriggerFilter.redirectToServlet("/servlet/org.makumba.parade.access.DatabaseLogServlet", record);
-        //TriggerFilter.redirectToServlet("perThreadEnabler", "perThreadEnabled", new Boolean(true));
         PerThreadPrintStream.setEnabled(true);
     }
     
