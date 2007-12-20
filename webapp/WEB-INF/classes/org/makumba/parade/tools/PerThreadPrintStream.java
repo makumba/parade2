82876c3398035407dfdb00345344601cd8464d0c
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2007-12-20 14:15:03 +0000

    formatting source

--- webapp/WEB-INF/classes/org/makumba/parade/tools/PerThreadPrintStream.java
+++ webapp/WEB-INF/classes/org/makumba/parade/tools/PerThreadPrintStream.java
@@ -12,7 +12,7 @@ public class PerThreadPrintStream extends java.io.PrintStream {
     public static DateFormat logDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     static Object dummy = "dummy";
-    
+
     public static PrintStream oldSystemOut;
 
     static ThreadLocal enable = new ThreadLocal() {
@@ -20,7 +20,7 @@ public class PerThreadPrintStream extends java.io.PrintStream {
             return true;
         }
     };
-    
+
     static ThreadLocal lastEnter = new ThreadLocal() {
         public Object initialValue() {
             return dummy;
@@ -47,65 +47,65 @@ public class PerThreadPrintStream extends java.io.PrintStream {
         super(o);
     }
 
-    public void write(byte[] buffer, int start, int len){
-            try {
-              
+    public void write(byte[] buffer, int start, int len) {
 
-        if(!(Boolean)enable.get()) {
-            super.write(buffer, start, len);
-            return;
-        }
-        String msg = new String(buffer, start, len);
+        try {
 
-        PerThreadPrintStreamLogRecord record = new PerThreadPrintStreamLogRecord();
-        record.setDate(new Date());
-        record.setMessage(msg);
-        
-        enable.set(false);
-        TriggerFilter.redirectToServlet("/servlet/org.makumba.parade.access.DatabaseLogServlet", record);
-        enable.set(true);
-        
-        String s = TriggerFilter.prefix.get();
-        byte[] b = null;
-        if (s != null)
-            b = (s + ": ").getBytes();
-        else //if s is null, it means that this didn't happen through some external access to the webapp
-            b = ("[Thread]  "+logDate.format(new Date()) + ": ").getBytes();
-        int lastStart = start;
-        if (b != null) {
-            for (int i = start; i < start + len; i++) {
-                if (buffer[i] == '\n') // FIXME, use general line separator
-                // here.
-                {
+            if (!(Boolean) enable.get()) {
+                super.write(buffer, start, len);
+                return;
+            }
+            String msg = new String(buffer, start, len);
+
+            PerThreadPrintStreamLogRecord record = new PerThreadPrintStreamLogRecord();
+            record.setDate(new Date());
+            record.setMessage(msg);
+
+            enable.set(false);
+            TriggerFilter.redirectToServlet("/servlet/org.makumba.parade.access.DatabaseLogServlet", record);
+            enable.set(true);
+
+            String s = TriggerFilter.prefix.get();
+            byte[] b = null;
+            if (s != null)
+                b = (s + ": ").getBytes();
+            else
+                // if s is null, it means that this didn't happen through some external access to the webapp
+                b = ("[Thread]  " + logDate.format(new Date()) + ": ").getBytes();
+            int lastStart = start;
+            if (b != null) {
+                for (int i = start; i < start + len; i++) {
+                    if (buffer[i] == '\n') // FIXME, use general line separator
+                    // here.
+                    {
+                        if (lastEnter.get() != null) {
+                            out.write(b, 0, b.length);
+                        }
+
+                        else
+                            lastEnter.set(dummy);
+                        out.write(buffer, lastStart, i - lastStart + 1);
+                        // w.write(buffer, lastStart, i - lastStart + 1);
+                        lastStart = i + 1;
+                    }
+                }
+                if (lastStart < start + len) {
                     if (lastEnter.get() != null) {
                         out.write(b, 0, b.length);
                     }
-                        
-                    else
-                        lastEnter.set(dummy);
-                    out.write(buffer, lastStart, i - lastStart + 1);
-                    //w.write(buffer, lastStart, i - lastStart + 1);
-                    lastStart = i + 1;
+
+                    lastEnter.set(null);
+                    out.write(buffer, lastStart, start + len - lastStart);
+                    // w.write(buffer, lastStart, start + len - lastStart);
                 }
+            } else {
+                out.write(buffer, start, len);
+                // w.write(buffer, start, len);
             }
-            if (lastStart < start + len) {
-                if (lastEnter.get() != null) {
-                    out.write(b, 0, b.length);
-                }
-                    
-                
-                lastEnter.set(null);
-                out.write(buffer, lastStart, start + len - lastStart);
-                //w.write(buffer, lastStart, start + len - lastStart);
-            }
-        } else {
-            out.write(buffer, start, len);
-            //w.write(buffer, start, len);
+        } catch (Throwable e) {
+            PrintWriter p = new PrintWriter(new OutputStreamWriter(out));
+            e.printStackTrace(p);
+            p.flush();
         }
-            } catch (Throwable e) {
-                PrintWriter p = new PrintWriter(new OutputStreamWriter(out));
-                e.printStackTrace(p);
-                p.flush();
-            }
     }
 }
