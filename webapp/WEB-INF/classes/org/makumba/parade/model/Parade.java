b7a5f003d96f1ac06352ca6cda9b8ac59c70165a
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2008-01-03 11:48:42 +0000

    Fixing bug with lock detection

--- webapp/WEB-INF/classes/org/makumba/parade/model/Parade.java
+++ webapp/WEB-INF/classes/org/makumba/parade/model/Parade.java
@@ -359,7 +359,7 @@ public class Parade {
                      * 
                      */
                     private boolean isLocked(String rootPath, String fileName, int mask) {
-                        String path = rootPath + java.io.File.separator + (fileName.indexOf(java.io.File.separator) > -1?fileName.substring(0, fileName.indexOf(java.io.File.separator)):"");
+                        String path = rootPath + (fileName.indexOf(java.io.File.separator) > -1?java.io.File.separator:"") + (fileName.indexOf(java.io.File.separator) > -1?fileName.substring(0, fileName.lastIndexOf(java.io.File.separator)):"");
                         
                         if(fileName.endsWith(CvsController.CVS_LOCK) && mask == JNotify.FILE_CREATED) {
                             // a lock was just created, we register the directory
