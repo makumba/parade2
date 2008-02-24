e01901de27e7f73484ca2edc9894159f72bb8ff7
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2008-02-24 17:53:01 +0000

    Removing obsolete and outdated files

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export CATALINA_HOME=/home/projects/parade/util-java/apache-tomcat-5.5.20
-export CATALINA_BASE=.
-
-cp ${CATALINA_HOME}/server/lib/catalina-ant.jar ${ANT_HOME}/lib
-
-ant prepareTomcat
