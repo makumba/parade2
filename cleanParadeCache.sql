b3ac97676a9aa29f7ba2b47b48fd1185d2f10c1e
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2008-03-29 14:22:34 +0000

    SQL script that cleans the db cache

---
+++ cleanParadeCache.sql
@@ -0,0 +1,14 @@
+drop table FILE_CVS;
+drop table FILE_TRACKER;
+drop table File;
+drop table FileData;
+drop table Parade;
+drop table ROW_ANT;
+drop table ROW_ANT_TARGETS;
+drop table ROW_CVS;
+drop table ROW_MAKUMBA;
+drop table ROW_TRACKER;
+drop table ROW_WEBAPP;
+drop table Row;
+drop table RowData;
+
