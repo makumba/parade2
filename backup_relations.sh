f97538a46d3dff41dd39443eeecdb4c44e3419fd
Manuel Bernhardt <bernhardt.manuel@gmail.com>
2008-05-23 14:36:31 +0000

    backup scripts for aether & relations

---
+++ backup_relations.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mysqldump -u root parade _org_makumba_devel_relations_relation _org_makumba_devel_relations_relation__origin _org_makumba_devel_relations_relationorigin _org_makumba_devel_relations_webappdatabase > backup_relations.sql 
+
---
+++ backup_rules.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mysqldump -u root parade InitialPercolationRule InitialPercolationRule__relationQueries PercolationRule PercolationRule__relationQueries RelationQuery MatchedAetherEvent PercolationStep > backup_relations.sql
