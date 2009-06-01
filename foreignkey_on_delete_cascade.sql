6f4f42263c75d2c6424366ffdb9920ca402e3cd9
Maarten De Smedt <maarten12@gmail.com>
2009-06-01 12:35:24 +0000

    Wouter: update the sql file, another foreign key set to cascade

--- foreignkey_on_delete_cascade.sql
+++ foreignkey_on_delete_cascade.sql
@@ -3,3 +3,9 @@ ALTER TABLE `PercolationStep` DROP FOREIGN KEY `FKB547075E29F27E9C` ;
 ALTER TABLE `PercolationStep` ADD FOREIGN KEY ( `root` ) REFERENCES `parade`.`PercolationStep` (
 `percolationstep`
 ) ON DELETE CASCADE ON UPDATE CASCADE ;
+
+ALTER TABLE `PercolationStep` DROP FOREIGN KEY `FKB547075EDDD0B8B1` ;
+
+ALTER TABLE `PercolationStep` ADD FOREIGN KEY ( `previous` ) REFERENCES `parade`.`PercolationStep` (
+`percolationstep`
+) ON DELETE CASCADE ON UPDATE CASCADE ;
