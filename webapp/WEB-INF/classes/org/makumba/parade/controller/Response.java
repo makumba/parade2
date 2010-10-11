5d780109f400a7eddc0f40b0320709336d8eeb6b
joaosa <jjadonotenter@gmail.com>
2010-10-11 20:41:33 +0000

    - created to unify response format to the DispatchAction layer.

---
+++ webapp/WEB-INF/classes/org/makumba/parade/controller/Response.java
@@ -0,0 +1,29 @@
+package org.makumba.parade.controller;
+
+/**
+ * 
+ * @author Joao Andrade
+ * 
+ *         Responsible for outputting correctly formated messages to the ActionForward layer
+ * 
+ */
+public class Response {
+
+    private String message;
+
+    public Response(String message) {
+        this.setMessage(message);
+    }
+
+    public Boolean isSuccess() {
+        return !getMessage().startsWith("Error");
+    }
+
+    private void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
