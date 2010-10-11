8cbe38383c76b60bc61690995a0d04c06eaaac8e
joaosa <jjadonotenter@gmail.com>
2010-10-11 20:54:47 +0000

    - Moved in method uploadFile from CommandAction.java as it is an action that doesn't affect files directly and is executed from the command window. - substituted the request parameter "params" for "file" (as it was redundant) - integrated Response.java

--- webapp/WEB-INF/classes/org/makumba/parade/controller/CommandAction.java
+++ webapp/WEB-INF/classes/org/makumba/parade/controller/CommandAction.java
@@ -15,15 +15,26 @@ public class CommandAction extends DispatchAction {
 
         String context = request.getParameter("context");
         String path = request.getParameter("path");
-        String[] params = {request.getParameter("params"), path};
+        String file = request.getParameter("file");
 
-        Object[] result = CommandController.onNewFile(context, params);
-        request.setAttribute("result", (String) result[0]);
-        request.setAttribute("success", (Boolean) result[1]);
+        Response result = CommandController.onNewFile(context, path, file);
+
+        Boolean success = result.isSuccess();
+        if (success) {
+            // FIXME re-enable Edit file link in a hack-free way
+            // result += " <a href='/File.do?op=editFile&context=" + context + "&path=" + path + "&file=" + filename
+            // + "&editor=codepress'>Edit</a></b>";
+            /*
+             * result += " <a href='simpleFileEditor.jsp?context=" + row.getRowname() + "&path=" + path + "&file=" +
+             * path + java.io.File.separator + filename + "&editor=codepress'>Edit</a></b>";
+             */
+        }
+
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", success);
         request.setAttribute("context", context);
         request.setAttribute("path", path);
         request.setAttribute("display", "file");
-
         return mapping.findForward("files");
     }
 
@@ -32,15 +43,43 @@ public class CommandAction extends DispatchAction {
 
         String context = request.getParameter("context");
         String path = request.getParameter("path");
-        String[] params = {request.getParameter("params"), path};
+        String file = request.getParameter("file");
 
-        Object[] result = CommandController.onNewDir(context, params);
-        request.setAttribute("result", (String) result[0]);
-        request.setAttribute("success", (Boolean) result[1]);
+        Response result = CommandController.onNewDir(context, path, file);
+
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", result.isSuccess());
         request.setAttribute("context", context);
         request.setAttribute("path", path);
         request.setAttribute("display", "file");
-
         return mapping.findForward("files");
+    }
+
+    public ActionForward upload(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        String context = request.getParameter("context");
+        String path = request.getParameter("path");
+
+        // Process the file upload form
+        UploadForm upload = (UploadForm) form;
+        String contentType = upload.getFileContentType();
+        String fileName = upload.getFileName();
+        int fileSize = upload.getFileSize();
+        byte[] fileData = upload.getFileData();
+
+        Response result = CommandController.onUpload(context, path, fileName, fileData);
+
+        Boolean success = result.isSuccess();
+        if (success) {
+            request.setAttribute("contentType", contentType);
+            request.setAttribute("contentLength", fileSize);
+        }
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", success);
+        request.setAttribute("context", context);
+        request.setAttribute("path", path);
+        request.setAttribute("file", fileName);
+        return mapping.findForward("uploadResponse");
     }
 }
