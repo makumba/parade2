56f30f88a2263ee8266a3dfc8da31b402d524c5c
joaosa <jjadonotenter@gmail.com>
2010-11-05 20:41:16 +0000

    - casts replaced by Response access methods - added saveFile

--- webapp/WEB-INF/classes/org/makumba/parade/controller/EditAction.java
+++ webapp/WEB-INF/classes/org/makumba/parade/controller/EditAction.java
@@ -12,9 +12,48 @@ public class EditAction extends DispatchAction {
 
     public ActionForward editFile(ActionMapping mapping, ActionForm form, HttpServletRequest request,
             HttpServletResponse response) throws Exception {
+        
+        String context = request.getParameter("context");
+        String path = request.getParameter("path");
+        String file = request.getParameter("file");
+        String editor = request.getParameter("editor");
 
-        return (mapping.findForward("files"));
-
+        request.setAttribute("context", context);
+        request.setAttribute("path", path);
+        request.setAttribute("file", file);      
+        return mapping.findForward(editor);
     }
 
+    public ActionForward revertFile(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        
+        String context = request.getParameter("context");
+        String path = request.getParameter("path");
+        String file = request.getParameter("file");
+        String editor = request.getParameter("editor");
+
+        request.setAttribute("context", context);
+        request.setAttribute("path", path);
+        request.setAttribute("file", file);      
+        return mapping.findForward(editor);
+    }
+    
+    public ActionForward saveFile(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+
+        String context = request.getParameter("context");
+        String path = request.getParameter("path");
+        String file = request.getParameter("file");
+        String editor = request.getParameter("editor");
+        String[] source = request.getParameterValues("source");
+
+        Response result = FileController.onSaveFile(context, path, file, source);
+
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", result.isSuccess());
+        request.setAttribute("context", context);
+        request.setAttribute("path", path);
+        request.setAttribute("file", file);
+        return mapping.findForward(editor);
+    }
 }
