1d7f432792077133a8988116c9c6b9bbc3bbaa4d
joaosa <jjadonotenter@gmail.com>
2010-11-05 20:39:04 +0000

    - integrated the Response class - Formatted the class to comply to DispatchAction - Removed the unneeded view parameter

--- webapp/WEB-INF/classes/org/makumba/parade/controller/WebappAction.java
+++ webapp/WEB-INF/classes/org/makumba/parade/controller/WebappAction.java
@@ -3,41 +3,108 @@ package org.makumba.parade.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.struts.action.Action;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
+import org.apache.struts.actions.DispatchAction;
 
-public class WebappAction extends Action {
+/**
+ * 
+ * @author Manuel Gay
+ * @author Joao Andrade
+ *
+ */
+public class WebappAction extends DispatchAction {
+    
+    private WebappController webappController = new WebappController();
 
-    @Override
-    public ActionForward execute(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+    public ActionForward servletContextStart(ActionMapping mapping, ActionForm form, HttpServletRequest request,
             HttpServletResponse response) throws Exception {
-
         String context = request.getParameter("context");
-        String op = request.getParameter("op");
-        String display = request.getParameter("display");
         String path = request.getParameter("path");
-        if (path == null)
-            path = (String) request.getAttribute("path");
-        if (path == null)
-            path = "";
-        String view = request.getParameter("view");
-        if (view == null)
-            view = (String) request.getAttribute("view");
-        if (view == null)
-            view = "commandOutput";
-
-        WebappController webappCtrl = new WebappController();
-        Object result[] = webappCtrl.onWebappAction(context, op);
-
-        request.setAttribute("result", result[0]);
-        request.setAttribute("success", result[1]);
+        String display = request.getParameter("display");
+        
+        Response result = webappController.onContextStart(context);
+        
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", result.isSuccess());
         request.setAttribute("context", context);
         request.setAttribute("path", path);
-        request.setAttribute("view", view);
-        request.setAttribute("display", display);
+        return mapping.findForward(display);
+    }
 
+    public ActionForward servletContextStop(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        String context = request.getParameter("context");
+        String path = request.getParameter("path");
+        String display = request.getParameter("display");
+        
+        Response result = webappController.onContextStop(context);
+        
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", result.isSuccess());
+        request.setAttribute("context", context);
+        request.setAttribute("path", path);
+        return mapping.findForward(display);
+    }
+
+    public ActionForward servletContextReload(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        String context = request.getParameter("context");
+        String path = request.getParameter("path");
+        String display = request.getParameter("display");
+        
+        Response result = webappController.onContextReload(context);
+        
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", result.isSuccess());
+        request.setAttribute("context", context);
+        request.setAttribute("path", path);
+        return mapping.findForward(display);
+    }
+
+    public ActionForward servletContextRemove(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        String context = request.getParameter("context");
+        String path = request.getParameter("path");
+        String display = request.getParameter("display");
+        
+        Response result = webappController.onContextRemove(context);
+        
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", result.isSuccess());
+        request.setAttribute("context", context);
+        request.setAttribute("path", path);
+        return mapping.findForward(display);
+    }
+
+    public ActionForward servletContextInstall(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        String context = request.getParameter("context");
+        String path = request.getParameter("path");
+        String display = request.getParameter("display");
+        
+        Response result = webappController.onContextInstall(context);
+        
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", result.isSuccess());
+        request.setAttribute("context", context);
+        request.setAttribute("path", path);
+        return mapping.findForward(display);
+    }
+
+    public ActionForward servletContextRedeploy(ActionMapping mapping, ActionForm form, HttpServletRequest request,
+            HttpServletResponse response) throws Exception {
+        String context = request.getParameter("context");
+        String path = request.getParameter("path");
+        String display = request.getParameter("display");
+        
+        Response result = webappController.onContextRedeploy(context);
+        
+        request.setAttribute("result", result.getMessage());
+        request.setAttribute("success", result.isSuccess());
+        request.setAttribute("context", context);
+        request.setAttribute("path", path);
         return mapping.findForward(display);
     }
 }
