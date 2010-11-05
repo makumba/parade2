7160dab2e8404f20ec43a5bdd3715e7d03e49ca3
joaosa <jjadonotenter@gmail.com>
2010-11-05 20:40:51 +0000

    - removed editFile - casts replaced by Response access methods

--- webapp/WEB-INF/classes/org/makumba/parade/controller/FileAction.java
+++ webapp/WEB-INF/classes/org/makumba/parade/controller/FileAction.java
@@ -51,38 +51,4 @@ public class FileAction extends DispatchAction {
         request.setAttribute("file", file);
         return mapping.findForward("files");
     }
-
-    /**
-     * @author Joao Andrade
-     * @param mapping
-     * @param form
-     * @param request
-     * @param response
-     * @return
-     * @throws Exception
-     */
-    public ActionForward editFile(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-            HttpServletResponse response) throws Exception {
-        return mapping.findForward("codePressEdit");
-    }
-
-    public ActionForward saveFile(ActionMapping mapping, ActionForm form, HttpServletRequest request,
-            HttpServletResponse response) throws Exception {
-
-        String context = request.getParameter("context");
-        String path = request.getParameter("path");
-        String file = request.getParameter("file");
-
-        String editor = request.getParameter("editor");
-        String[] source = request.getParameterValues("source");
-
-        Response result = FileController.onSaveFile(context, path, file, source);
-
-        request.setAttribute("result", result.getMessage());
-        request.setAttribute("success", result.isSuccess());
-        request.setAttribute("context", context);
-        request.setAttribute("path", path);
-        request.setAttribute("file", file);
-        return mapping.findForward(editor + "Edit");
-    }
 }
